1ec1f3f6e5e80ec2b14ffd7afca7fd6a0aea92a8
steverova <71293149+steverova@users.noreply.github.com>
2022-11-18 20:26:42 -0600

    machine learning dt

--- src/main.py
+++ src/main.py
@@ -1,24 +1,24 @@
 
 from fastapi import FastAPI
-from services import CattleServices
 from pydantic import BaseModel
 
-import json
+import services as services
 
 app = FastAPI()
-
 
 class Item(BaseModel):
     Year: int
     Month: int
-    Weight: str
-    Gender: str
-    Price = float
+    Gender: int
 
 
-@app.get("/", response_model=list[Item])
-async def getData():
-    services = CattleServices()
-    list = await services.getData()
-    print(list)
-    return list
+@app.post("/")
+def getDataCattle(item: Item):
+    
+    json_data = services.getData()
+    services.create_csv(json_data)
+    csv_path = './data_cattle.csv'
+    dtree = services.calculate_prediction(csv_path)
+    result = services.predict(dtree, item)
+
+    return result
--- src/services.py
+++ src/services.py
@@ -1,14 +1,55 @@
 from database import Connection
+import json
+import csv
+import pandas
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
 
-class CattleServices: 
+def getData():
+    cursor = Connection()
+    cursor.execute("exec [dbo].[SP_AVERAGE_PRICE_CATTLE_SUMMARY]")
+    rows = cursor.fetchall()
+    cattleList = []
+    for row in rows:
+        cattleList.append({"Year": row[0], "Month": row[1], "Gender": row[2], "Price": row[3]})
+    return cattleList
+
+
+def create_csv(json_data):
+    write_csv = open('./data_cattle.csv', 'w')
+    csv_writer = csv.writer(write_csv)
     
-    def getData():
-        print("enter")
-        cattleList = []
-        cursor = Connection()
-        cursor.execute("exec [dbo].[SP_AVERAGE_PRICE_CATTLE_SUMMARY]")
-        rows = cursor.fetchall()
-        for row in rows:
-            cattleList.append(row) 
-        return cattleList   
-            
+    count = 0
+    
+    for item in json_data:
+        if count == 0:
+            header = item.keys()
+            csv_writer.writerow(header)
+            count += 1
+        csv_writer.writerow(item.values())
+    write_csv.close()
+    
+    
+def calculate_prediction(csv_path):
+    
+    df = pandas.read_csv(csv_path)
+
+    gender_q = {'Female': 0, 'Male': 1}
+    df['Gender'] = df['Gender'].map(gender_q)
+
+
+    attributes = ['Year','Month','Gender']
+    x = df[attributes]
+    y = df['Price']
+
+    dtree = DecisionTreeClassifier()
+    dtree = dtree.fit(x, y) 
+    tree.plot_tree(dtree, feature_names=attributes)
+    
+    return dtree
+
+
+def predict(dtree, item):
+    result = int(dtree.predict([[item.Year, item.Month, item.Gender]]))
+    return result
+    
